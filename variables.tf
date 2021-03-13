1919f6e29eb28ce3f1b2d6a57fb424ef9922e6ee
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-03-13 22:34:50 +0100

    update

--- variables.tf
+++ variables.tf
@@ -27,74 +27,74 @@ variable "compartment_id" {
   type        = string
 }
 
-variable "label_prefix" {
-  description = "A string that will be prepended to all resources"
-  type        = string
-  default     = "none"
-}
+# variable "label_prefix" {
+#   description = "A string that will be prepended to all resources"
+#   type        = string
+#   default     = "none"
+# }
 
-variable "resource_name" {
-  description = "user-friendly string to name all resource. If undefined it will be derived from compartment name. "
-  type        = string
-  default     = ""
-}
+# variable "resource_name" {
+#   description = "user-friendly string to name all resource. If undefined it will be derived from compartment name. "
+#   type        = string
+#   default     = ""
+# }
 
-variable "tags" {
-  description = "A simple key-value pairs to tag the resources created"
-  type        = map(any)
-  default     = {}
-}
+# variable "tags" {
+#   description = "A simple key-value pairs to tag the resources created"
+#   type        = map(any)
+#   default     = {}
+# }
 
 # VCN parameters ------------------------------------------------------------
-variable "internet_gateway_enabled" {
-  description = "whether to create the internet gateway"
-  default     = true
-  type        = bool
-}
+# variable "internet_gateway_enabled" {
+#   description = "whether to create the internet gateway"
+#   default     = true
+#   type        = bool
+# }
 
-variable "nat_gateway_enabled" {
-  description = "whether to create a nat gateway in the vcn"
-  default     = true
-  type        = bool
-}
+# variable "nat_gateway_enabled" {
+#   description = "whether to create a nat gateway in the vcn"
+#   default     = true
+#   type        = bool
+# }
 
-variable "service_gateway_enabled" {
-  description = "whether to create a service gateway"
-  default     = false
-  type        = bool
-}
+# variable "service_gateway_enabled" {
+#   description = "whether to create a service gateway"
+#   default     = false
+#   type        = bool
+# }
 
-variable "vcn_cidr" {
-  description = "cidr block of VCN"
-  default     = "10.0.0.0/16"
-  type        = string
-}
+# variable "vcn_cidr" {
+#   description = "cidr block of VCN"
+#   default     = "10.0.0.0/16"
+#   type        = string
+# }
 
-# Public Subnet parameters --------------------------------------------------
-variable "public_netnum" {
-  description = "0-based index of the bastion subnet when the VCN's CIDR is masked with the corresponding newbit value."
-  default     = 0
-  type        = number
-}
+# # Public Subnet parameters --------------------------------------------------
+# variable "public_netnum" {
+#   description = "0-based index of the bastion subnet when the VCN's CIDR is masked with the corresponding newbit value."
+#   default     = 0
+#   type        = number
+# }
 
-variable "public_newbits" {
-  description = "The difference between the VCN's netmask and the desired bastion subnet mask"
-  default     = 8
-  type        = number
-}
+# variable "public_newbits" {
+#   description = "The difference between the VCN's netmask and the desired bastion subnet mask"
+#   default     = 8
+#   type        = number
+# }
 
-# Private Subnet parameters -------------------------------------------------
-variable "private_netnum" {
-  description = "0-based index of the private subnet when the VCN's CIDR is masked with the corresponding newbit value."
-  default     = 1
-  type        = number
-}
+# # Private Subnet parameters -------------------------------------------------
+# variable "private_netnum" {
+#   description = "0-based index of the private subnet when the VCN's CIDR is masked with the corresponding newbit value."
+#   default     = 1
+#   type        = number
+# }
 
-variable "private_newbits" {
-  description = "The difference between the VCN's netmask and the desired private subnet mask"
-  default     = 8
-  type        = number
-}
+# variable "private_newbits" {
+#   description = "The difference between the VCN's netmask and the desired private subnet mask"
+#   default     = 8
+#   type        = number
+# }
 
 # Trivadis LAB specific parameter -------------------------------------------
 variable "tvd_participants" {
@@ -103,27 +103,27 @@ variable "tvd_participants" {
   default     = 1
 }
 
-variable "tvd_domain" {
-  description = "The domain name of the LAB environment"
-  type        = string
-  default     = "trivadislabs.com"
-}
+# variable "tvd_domain" {
+#   description = "The domain name of the LAB environment"
+#   type        = string
+#   default     = "trivadislabs.com"
+# }
 
-variable "tvd_dns_hostnum" {
-  description = "The host number for the Trivadis LAB DNS server. This number is used to build the IP address using cidrhost function"
-  type        = number
-  default     = 4
-}
+# variable "tvd_dns_hostnum" {
+#   description = "The host number for the Trivadis LAB DNS server. This number is used to build the IP address using cidrhost function"
+#   type        = number
+#   default     = 4
+# }
 
-variable "tvd_private_dns" {
-  description = "A private DNS IP address for the training environment"
-  type        = string
-  default     = "default"
-}
+# variable "tvd_private_dns" {
+#   description = "A private DNS IP address for the training environment"
+#   type        = string
+#   default     = "default"
+# }
 
-variable "tvd_public_dns" {
-  description = "A public DNS IP address for the training environment"
-  type        = string
-  default     = "8.8.8.8"
-}
+# variable "tvd_public_dns" {
+#   description = "A public DNS IP address for the training environment"
+#   type        = string
+#   default     = "8.8.8.8"
+# }
 # --- EOF -------------------------------------------------------------------
